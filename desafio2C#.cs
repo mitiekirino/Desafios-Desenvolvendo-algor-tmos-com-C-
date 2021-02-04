0142518198d06fbb42bb5b95be2b63ec896a0fb2
mitiekirino <mitiekirino@gmail.com>
2021-02-03 21:52:41 -0300

    Desafio 2

---
+++ desafio2C#.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Linq;
+
+class Program {
+  static void Main(string[] args) {
+    int numeroDeFigurinhas = Int32.Parse(Console.ReadLine());
+    int numeroDeFigurinhasCompradas = Int32.Parse(Console.ReadLine());
+    int totalDeFigurinhas = 0;
+
+    int[] albumDeFigurinha = new int[numeroDeFigurinhasCompradas] ;
+
+    for (int i = 0; i < numeroDeFigurinhasCompradas; i++) {
+      int figurinha = int.Parse(Console.ReadLine());
+        if (!albumDeFigurinha.Contains(figurinha)) {
+          albumDeFigurinha[i] = figurinha;
+          numeroDeFigurinhas -= 1;
+        }
+        if (numeroDeFigurinhas == 0) {
+          break;
+        }
+    }
+    totalDeFigurinhas = numeroDeFigurinhas;
+
+    Console.WriteLine(totalDeFigurinhas);
+  }
+}
